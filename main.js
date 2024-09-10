6388874776d1aa8a0954b814a4a756b8f83ebd04
ahmed magdy <ahmedmagdee04@gmail.com>
2024-09-10 07:43:35 +0300

    Edit on JS

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ fetch("https://api.currencyfreaks.com/v2.0/rates/latest?apikey=2329f856815043c79
     const egpPrice = document.querySelector(".egp span");
     const sarPrice = document.querySelector(".sar span");
 
-    // Update prices when the amount changes
+    
     amountInput.addEventListener("input", () => {
-      const amount = parseFloat(amountInput.value) || 0; // Get the input value and handle empty or invalid values
+      const amount = parseFloat(amountInput.value) || 0; 
       const egpRate = parseFloat(currencyData.rates["EGP"]);
       const sarRate = parseFloat(currencyData.rates["SAR"]);
 
-      // Update the displayed prices
-      egpPrice.innerHTML = (amount * egpRate).toFixed(2); // Multiply amount by EGP rate and format to 2 decimal places
-      sarPrice.innerHTML = (amount * sarRate).toFixed(2); // Multiply amount by SAR rate and format to 2 decimal places
+      
+      egpPrice.innerHTML = (amount * egpRate).toFixed(2); 
+      sarPrice.innerHTML = (amount * sarRate).toFixed(2); 
     });
   })
   .catch((error) => {
